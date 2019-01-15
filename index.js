3a3d02c96105eb5ec0f30bc3e8b0f9627d7463b6
Neil Kalman <Neilkalman@gmail.com>
2019-01-15 23:47:54 +0200

    feat(events): listen to the events related to tdd1t actions

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const lintPR = require('./lib/lint-pull-request');
+const checkPrTDD = require('./lib/check-pr-tdd');
 
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!');
+  app.log('Yay, tdd1t was loaded!');
 
-  app.on("pull_request.opened", lintPR);
-  app.on("pull_request.synchronize", lintPR);
+  app.on('pull_request.opened', checkPrTDD);
+  app.on('pull_request.synchronize', checkPrTDD);
+
+  app.on('issues.edited', checkPrTDD);
 
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-}
+};
