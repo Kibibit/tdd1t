f1c173b1ffbb2bac134311a5796e7be29775f307
Neil Kalman <Neilkalman@gmail.com>
2019-01-16 03:37:24 +0200

    docs(homepage): show different subtitle on first install
    this is just the first step. We might change a few more things based on first installation (how to
    use, etc.). This will happen after we'll use a documentation theme instead of a static html file

--- docs/js/index.js
+++ docs/js/index.js
@@ -4,3 +4,20 @@ setTimeout(() => {
 }, 1500);
 
 hljs.initHighlightingOnLoad();
+
+// TODO: We might change more elements based on first installation
+// like including more specific documentation
+if (getUrlVars().setup_action === 'install') {
+  $('#kb-main-subtitle').text('Thanks for integrating tdd1t');
+}
+
+function getUrlVars() {
+  var vars = {};
+
+  window.location.href
+    .replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+      vars[key] = value;
+    });
+
+  return vars;
+}
